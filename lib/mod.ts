afea3d6055f3740a706add1c03832bd85fecf80a
peanutbother <peanutbother@proton.me>
2023-03-29 13:22:41 +0200

    fix export paths

--- lib/mod.ts
+++ lib/mod.ts
@@ -1,4 +1,4 @@
 export * as Types from "./types.ts";
-export * as Transformers from "./transformers/mod.ts";
-export * as Tags from "./tags/mod.ts";
+export * as Transformers from "./transformers.ts";
+export * as Tags from "./tags.ts";
 export * as Utils from "./utils.ts";
